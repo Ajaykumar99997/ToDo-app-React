c1d4120b8b1e52708edf48306ce66e3b8d324c25
Ajay kumar P V <ajaykumar1997.ak@gmail.com>
2022-10-23 10:07:27 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+
+  toDos.forEach((item, i) => {
+    item.id = i + 1;
+  });
+
+  let remove = (e) => {
+    let x = e.target.getAttribute('removetodo');
+    setToDos(toDos.filter((items) => items.text !== x));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() => setToDos([...toDos, { text: toDo, status: false }])}
+          className="fas fa-plus"
+        ></i>
+      </div>
+      <div className="todos">
+        {toDos.map((obj) => {
+          return (
+            <div className="todo">
+              <div className="left">
+                <input
+                  onChange={(e) => {
+                    setToDos(
+                      toDos.filter((obj2) => {
+                        if (obj2.id === obj.id) {
+                          obj2.status = e.target.checked;
+                        }
+                        return obj2;
+                      })
+                    );
+                  }}
+                  value={obj.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i
+                  removetodo={obj.text}
+                  onClick={remove}
+                  // onClick={() =>
+                  //   setToDos((obj) => obj.filter((_, index) => index !== 0))
+                  // }
+                  className="fas fa-times"
+                ></i>
+              </div>
+            </div>
+          );
+        })}
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return <h1>{obj.text}</h1>;
+          }
+          return null;
+        })}
+      </div>
     </div>
   );
 }
